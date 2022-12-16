6f71e8637f4979c4df136522f62d14c29f441f28
kyle.heller <kyle.heller@gmail.com>
2022-12-16 11:50:56 -0500

    Added roman to integer recursive function

--- recursion.rb
+++ recursion.rb
@@ -127,3 +127,32 @@ roman_hash = {
       end
     end
   end
+
+#   Use the roman_mapping hash to define a recursive method that converts a Roman numeral to an integer.
+
+roman_mapping = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
+
+def roman_to_integer(roman_mapping, str, result = 0)
+  return result if str.empty?
+  roman_mapping.keys.each do |roman|
+    if str.start_with?(roman)
+      result += roman_mapping[roman]
+      str = str.slice(roman.length, str.length)
+      return roman_to_integer(roman_mapping, str, result)
+    end
+  end
+end
